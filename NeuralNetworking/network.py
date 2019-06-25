b31980e4ea646690acb547248f686fba4eeb004b
acuddeback <atc@cuddeback.org>
2019-06-25 17:03:23 -0400

    changed file structure

--- NeuralNetworking/network.py
+++ NeuralNetworking/network.py
@@ -15,7 +15,7 @@ class Network:
     wasTested = False	# boolean which sais whether or not the network was tested while training, used for plotting
     
     #     n_epochs	# number of epochs, inputted as train param
-    #     lr = 0.01 # learning rate, train parameter
+    lr = 0.01 # learning rate, train parameter
     #     train_data = []	# inputted as train param
     #     train_labels = []	# inputted as train param
     #     test_data = []	# inputted as train param
@@ -25,17 +25,17 @@ class Network:
     
     # methods
     def __init__(self, _nodes):
-        nodes = _nodes # set array of nodes
-        num_layers = len(_nodes)
+        self.nodes = _nodes # set array of nodes
+        self.num_layers = len(_nodes)
 #         n_hidden = len(_nodes)-2
         
         # initialize weights 
-        weights = []
+        self.weights = []
         for i in range(len(_nodes)-1):
             w = torch.randn(_nodes[i], _nodes[i+1])
             weights.append(w)
         # initialize biases
-        biases = []
+        self.biases = []
         for i in range(len(nodes)-1):
             b = torch.randn(1, nodes[i+1])
             biases.append(b)   
@@ -78,7 +78,6 @@ class Network:
             dd = loss * d
             loss_d = torch.mm(d, w.t())
             ds.append(dd)
-        learning_rate = 0.1
         _as = copy.deepcopy(_layer_vals)
         _as.insert(0,x)
         del _as[-1]
@@ -87,18 +86,18 @@ class Network:
         new_biases = []
         print("lengths", len(ds), len(_weights), len(_as))
         for d, w, a, b in zip(ds, reversed(_weights), _as, reversed(_biases)):
-            wt = torch.mm(a.t(),d) * learning_rate
+            wt = torch.mm(a.t(),d) * lr
             new_weights.insert(0, wt)
 
-            bi = b + d.sum()*learning_rate
+            bi = b + d.sum()*lr
             new_biases.insert(0,bi)
         return new_weights,  new_biases  
 
     def train(self, _train_data, _train_labels, _n_epochs, _lr=0.01, _test=False, _test_data=[], _test_labels=[]):
-        
+    self.lr = _lr
     for i in range(_n_epochs):
         y_out = forward_prop(_data, _nodes, weights, biases)
-        weights, biases = backprop_and_update(y_out, weights, biases, _data)
+        self.weights, self.biases = backprop_and_update(y_out, weights, biases, _data)
 
 
         
