7d43dc94e7d611a7e9862582da0aa08050facab8
acuddeback <atc@cuddeback.org>
2019-06-26 10:06:03 -0400

    renamed pytorch notebook, added to network.py

--- NeuralNetworking/network.py
+++ NeuralNetworking/network.py
@@ -13,6 +13,7 @@ class Network:
     output = []	# initialized as empty array, updated through training
     loss = [] 	# current loss value, train_labels - output
     wasTested = False	# boolean which sais whether or not the network was tested while training, used for plotting
+    layers = [] # layer vals
     
     #     n_epochs	# number of epochs, inputted as train param
     lr = 0.01 # learning rate, train parameter
@@ -21,7 +22,15 @@ class Network:
     #     test_data = []	# inputted as train param
     #     test_labels = []	## inputted as train param
 
+    def get_output(self):
+        return self.layers[-1]
     
+    def get_accuracy(self, _data, _labels):
+        m = _labels.shape[0]
+        pred = (self.pred(_data))[-1] # last index of layer vals
+        pred = pred.reshape(_labels.shape)
+        error = np.sum(np.abs(pred-_labels))
+        return ((m-error)/m) * 100
     
     # methods
     def __init__(self, _nodes):
@@ -44,60 +53,72 @@ class Network:
     def sigmoid_activation(z):
         return 1 / (1 + torch.exp(-z))    
     
-    def forward_prop(_data, _nodes, _weights, _biases):
+    def pred(self, _data):
         layer_vals = []
         a = _data
-        for i in range(len(_nodes)-2): # feed thru hidden layers
-            z = torch.mm(a, _weights[i]) + _biases[i]
+        for i in range(len(self.nodes)-2): # feed thru hidden layers
+            z = torch.mm(a, self.weights[i]) + self.biases[i]
             a = sigmoid_activation(z)
             layer_vals.append(a)
-        z_out = torch.mm(a, _weights[-1]) + _biases[-1]
+        z_out = torch.mm(a, self.weights[-1]) + self.biases[-1]
         output = sigmoid_activation(z_out)
         layer_vals.append(output)
     #     print(output)
-        return layer_vals
+        return layer_vals 
+        
+    def forward_prop(self, _data):
+        self.layers = forward_prop(self, data)
     
     # loss computation
     # loss = y - output
-    def calculate_loss(_layer_vals):
-        return y - _layer_vals[-1]
+    def calculate_loss(self, _labels):
+        return _labels - self.layers[-1]
     
     ## function to calculate the derivative of activation
     def sigmoid_delta(x):
-      return x * (1 - x)
+        return x * (1 - x)
 
-    def backprop_and_update(_layer_vals, _weights, _biases, _data):
-        loss = calculate_loss(_layer_vals)
+    def backprop_and_update(sefl, _data, _labels):
+        loss = calculate_loss(_labels)
         deltas = [] # from first hidden to output
         ds = [] # from output to first hidden
         # compute derivative of error terms
-        for a in _layer_vals:
+        for a in self.layers:
             deltas.append(sigmoid_delta(a))
         loss_d = loss
-        for d, w in zip(reversed(deltas), reversed(_weights)):
+        for d, w in zip(reversed(deltas), reversed(self.weights)):
             dd = loss * d
             loss_d = torch.mm(d, w.t())
             ds.append(dd)
-        _as = copy.deepcopy(_layer_vals)
+        _as = copy.deepcopy(self.layers)
         _as.insert(0,x)
         del _as[-1]
     #     print("\n Layer vals \n", _layer_vals)
         new_weights = []
         new_biases = []
-        print("lengths", len(ds), len(_weights), len(_as))
-        for d, w, a, b in zip(ds, reversed(_weights), _as, reversed(_biases)):
+        print("lengths", len(ds), len(self.weights), len(_as))
+        for d, w, a, b in zip(ds, reversed(self.weights), _as, reversed(self.biases)):
             wt = torch.mm(a.t(),d) * lr
             new_weights.insert(0, wt)
 
             bi = b + d.sum()*lr
             new_biases.insert(0,bi)
-        return new_weights,  new_biases  
+        self.weights = new_weights
+        self.biases = new_biases
+        
 
     def train(self, _train_data, _train_labels, _n_epochs, _lr=0.01, _test=False, _test_data=[], _test_labels=[]):
     self.lr = _lr
     for i in range(_n_epochs):
-        y_out = forward_prop(_data, _nodes, weights, biases)
-        self.weights, self.biases = backprop_and_update(y_out, weights, biases, _data)
+        self.forward_prop(_train_data)
+        # test here 
+        if (i%50 == 0):
+            print("train accuracy is:", get_accuracy(_train_data, _train_labels))
+            if(_test=True):
+                print("train accuracy is:", get_accuracy(_test_data, _test_labels))
+        self.backprop_and_update(_train_data, _train_labels)
+
+        
 
 
         
