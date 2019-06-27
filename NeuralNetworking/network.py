190300bf85f67204f205fa5eb50f7ecd06085c6f
acuddeback <atc@cuddeback.org>
2019-06-27 10:34:24 -0400

    fixed a lot of bugs BUT also not others

--- NeuralNetworking/network.py
+++ NeuralNetworking/network.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import copy
+import numpy as np
 
 class Network: 
     
@@ -34,8 +35,11 @@ class Network:
     def get_accuracy(self, _data, _labels):
         m = _labels.shape[0]
         pred = (self.pred(_data))[-1] # last index of layer vals
+        print("pred shape", pred.shape)
+        print("data shape:", _data.shape)
+        print("label shape:", _labels.shape)
         pred = pred.reshape(_labels.shape)
-        error = np.sum(np.abs(pred-_labels))
+        error = torch.sum(torch.abs(pred.float()-_labels.float()))
         return ((m-error)/m) * 100
     
     # methods
@@ -43,7 +47,10 @@ class Network:
         self.nodes = _nodes # set array of nodes
         self.num_layers = len(_nodes)
 #         n_hidden = len(_nodes)-2
-        
+            
+        for w in self.weights:
+            print("weight shape:", w.shape)
+    
         # initialize weights 
         self.weights = []
         for i in range(len(self.nodes)-1):
@@ -56,22 +63,24 @@ class Network:
             self.biases.append(b)   
             
     ## sigmoid activation function using pytorch
-    def sigmoid_activation(z):
+    def sigmoid_activation(self, z):
         return 1 / (1 + torch.exp(-z))    
     
     def pred(self, _data):
         layer_vals = []
-        a = torch.from_numpy(_data)
-        a = a.double()
-        print("a is \n", a)
+        a = _data
+        a = a.float()
+#         print("a is \n", a)
         for i in range(len(self.nodes)-2): # feed thru hidden layers
             print(self.weights[i])
 #             wt = self.weights[i].clone()
+            print("a shape:", a.shape)
+            print("weights", i, "shape:", self.weights[i].shape)
             z = torch.mm(a, self.weights[i]) + self.biases[i]
-            a = sigmoid_activation(z)
+            a = self.sigmoid_activation(z)
             layer_vals.append(a)
         z_out = torch.mm(a, self.weights[-1]) + self.biases[-1]
-        output = sigmoid_activation(z_out)
+        output = self.sigmoid_activation(z_out)
         layer_vals.append(output)
     #     print(output)
         return layer_vals 
@@ -82,36 +91,37 @@ class Network:
     # loss computation
     # loss = y - output
     def calculate_loss(self, _labels):
-        return _labels - self.layers[-1]
+        return _labels.float() - self.layers[-1].float()
     
     ## function to calculate the derivative of activation
-    def sigmoid_delta(x):
+    def sigmoid_delta(self, x):
         return x * (1 - x)
 
-    def backprop_and_update(sefl, _data, _labels):
-        loss = calculate_loss(_labels)
+    def backprop_and_update(self, _data, _labels):
+        loss = self.calculate_loss(_labels)
         deltas = [] # from first hidden to output
         ds = [] # from output to first hidden
         # compute derivative of error terms
         for a in self.layers:
-            deltas.append(sigmoid_delta(a))
+            deltas.append(self.sigmoid_delta(a))
         loss_d = loss
         for d, w in zip(reversed(deltas), reversed(self.weights)):
             dd = loss * d
             loss_d = torch.mm(d, w.t())
             ds.append(dd)
         _as = copy.deepcopy(self.layers)
-        _as.insert(0,x)
+        _as.insert(0,_data) # insert input into the layer vals
         del _as[-1]
     #     print("\n Layer vals \n", _layer_vals)
+        # Update parameters
         new_weights = []
         new_biases = []
         print("lengths", len(ds), len(self.weights), len(_as))
         for d, w, a, b in zip(ds, reversed(self.weights), _as, reversed(self.biases)):
-            wt = torch.mm(a.t(),d) * lr
+            wt = torch.mm(a.t().float(),d.float()) * self.lr
             new_weights.insert(0, wt)
 
-            bi = b + d.sum()*lr
+            bi = b + d.sum()*self.lr
             new_biases.insert(0,bi)
         self.weights = new_weights
         self.biases = new_biases
@@ -123,9 +133,9 @@ class Network:
             self.forward_prop(_train_data)
             # test here 
             if (i%50 == 0):
-                print("train accuracy is:", get_accuracy(_train_data, _train_labels))
+                print("train accuracy is:", self.get_accuracy(_train_data, _train_labels))
                 if(_test==True):
-                    print("train accuracy is:", get_accuracy(_test_data, _test_labels))
+                    print("train accuracy is:", self.get_accuracy(_test_data, _test_labels))
             self.backprop_and_update(_train_data, _train_labels)
 
         
