7d03270d6da6011121efb09b946590911ae1c3f1
Benjamin Tannenwald <benjamin.tannenwald@cern.ch>
2019-09-03 16:29:25 -0400

    added condor submission files. standalone has error for sl7

---
+++ samples_500k/madgraphGenerate.csh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+echo "Starting job on " `date` #Date/time of start of job
+echo "Running on: `uname -a`" #Condor job is running on this node
+echo "System software: `cat /etc/redhat-release`" #Operating System on that node
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_92/x86_64-centos7-gcc62-opt/setup.csh
+xrdcp -s root://cmseos.fnal.gov//store/user/benjtann/upgrade/madgraph5/MG5_aMC_v2.6.6.tar.gz .
+xrdcp -s root://cmseos.fnal.gov//store/user/benjtann/upgrade/delphes/PhaseII/MinBias_100k.pileup .
+xrdcp -s root://cmseos.fnal.gov//store/user/benjtann/upgrade/madgraph5/installPackages1.cmd .
+xrdcp -s root://cmseos.fnal.gov//store/user/benjtann/upgrade/madgraph5/installPackages2.cmd .
+xrdcp -s root://cmseos.fnal.gov//store/user/benjtann/upgrade/samples/$1/$2 .
+
+#cp ../MG5_aMC_v2.6.6.tar.gz .
+tar -xf MG5_aMC_v2.6.6.tar.gz
+rm MG5_aMC_v2.6.6.tar.gz
+cd MG5_aMC_v2_6_6
+
+cp ../installPackages*.cmd . #some line to wget install script or just pass via condor submission
+cp ../$2 . #some line to auto-generate script
+
+echo "----> Install collier ninja"
+./bin/mg5_aMC installPackages1.cmd
+echo "----> Install Pythia8 Delphes ExRootAnalysis"
+./bin/mg5_aMC installPackages2.cmd
+echo "----> Copy over Delphes .tcl files"
+cp Delphes/cards/CMS_PhaseII/muonMomentumResolution.tcl Template/Common/Cards/
+cp Delphes/cards/CMS_PhaseII/trackMomentumResolution.tcl Template/Common/Cards/
+cp Delphes/cards/CMS_PhaseII/muonTightId.tcl Template/Common/Cards/
+cp Delphes/cards/CMS_PhaseII/muonLooseId.tcl Template/Common/Cards/
+echo "----> sed Delphes card for pileup location"
+sed -s 's/\/eos\/cms\/store\/group\/upgrade\/delphes\/PhaseII/../g' Delphes/cards/CMS_PhaseII/CMS_PhaseII_0PU_v02.tcl
+echo "----> Try generating some events?"
+./bin/mg5_amc $2
+
+### Now that the run is over, there is one or more root files created
+echo "List all root files = "
+#ls $1/*.root
+ls ./*/Events/run_01*/*.root
+echo "List all run files"
+ls ./*/Events/run_01*/*
+echo "*******************************************"
+OUTDIR=root://cmseos.fnal.gov//store/user/benjtann/upgrade/samples/$1/
+echo "xrdcp output for condor"
+for FILE in ./*/Events/run_01*/*.root
+do
+  echo "xrdcp -f ${FILE} ${OUTDIR}/${FILE}"
+  xrdcp -f ${FILE} ${OUTDIR}/${FILE} 2>&1
+  XRDEXIT=$?
+  if [[ $XRDEXIT -ne 0 ]]; then
+    rm *.root
+    echo "exit code $XRDEXIT, failure in xrdcp"
+    exit $XRDEXIT
+  fi
+  rm ${FILE}
+done
+
+cd ${_CONDOR_SCRATCH_DIR}
+rm -rf MG5_aMC_v2_6_6
+
---
+++ samples_500k/submitSampleToCondor.py
@@ -0,0 +1,120 @@
+# /usr/bin/python
+
+#Author: Ben Tannenwald
+#Date: August 5th, 2019
+#Purpose: Script to submit condor jobs for all files in sample
+
+import os,sys, argparse
+
+# *** 0. setup parser for command line
+parser = argparse.ArgumentParser()
+parser.add_argument("--outputDir", help="output directory for processed histograms and roofiles")
+parser.add_argument("--inputCMDFile", help=".cmd file containing generation command")
+parser.add_argument("--nTotalEvents", help="total number of events to generate", default='5000')
+parser.add_argument("--nEventsPerJob", help="number of events to run per job... use if you want to split a large generation into several sub-runs", default='1000')
+args = parser.parse_args()
+
+if ( not (len(vars(args)) != 2 or len(vars(args)) != 3) or len(vars(args)) != 4) ): # 2 OR 3 --> three for options
+    os.system('python submitSampleToCondor.py -h')
+    quit()
+
+# ** A. Test output directory existence and create if DNE
+if(args.outputDir is None):
+    print "#### Need to specify output directory using --outputDir <desired output directory> ####\nEXITING"
+    quit()
+else:
+    if ( not os.path.exists(args.outputDir) ):
+        print "Specified output directory ({0}) DNE.\nCREATING NOW".format(args.outputDir)
+        os.system("mkdir {0}".format(args.outputDir))
+
+    if ( not os.path.exists( (args.outputDir + '/condor_logs/') ) ):
+        os.system("mkdir {0}".format( (args.outputDir + '/condor_logs/')) )
+    if ( not os.path.exists( (args.outputDir + '/condor_err/') ) ):
+        os.system("mkdir {0}".format( (args.outputDir + '/condor_err/')) )
+    if ( not os.path.exists( (args.outputDir + '/condor_out/') ) ):
+        os.system("mkdir {0}".format( (args.outputDir + '/condor_out/')) )
+
+    print '-- Setting outputDir = {0}'.format(args.outputDir)
+
+# ** B. Test input .txt file and exit if DNE
+if(args.inputCMDFile is None):
+    print "#### Need to specify input .cmd file using --inputCMDFile <address to .cmd file> ####\nEXITING\n"
+    quit()
+else:
+    if ( not os.path.exists(args.inputCMDFile) ):
+        print "#### Specified input file ({0}) DNE ####.\nEXITING\n".format(args.inputCMDFile)
+        quit()
+    else:
+        print '-- Setting inputCMDFile = {0}'.format(args.inputCMDFile)
+
+# ** C. Test nEventsPerJob flag and exit if not sensible
+if (args.nEventsPerJob).isdigit() is True:
+    print "-- Running with {0} events per job ####\n".format(args.nEventsPerJob)
+else:
+    print "#### WARNING: Passed option of {0} files per job makes no sense. DNE ####\nEXITING\n".format(args.nEventsPerJob)
+    quit()
+
+# ** C. Test nTotalEvents flag and exit if not sensible
+if (args.nTotalEvents).isdigit() is True:
+    print "-- Running with {0} events total ####\n".format(args.nTotalEvents)
+else:
+    print "#### WARNING: Passed option of {0} total events makes no sense. DNE ####\nEXITING\n".format(args.nTotalEvents)
+    quit()
+
+# FIXME, 05-08-19 BBT
+## ** D. Exit if no grid proxy
+#if ( not os.path.exists(os.path.expandvars("$X509_USER_PROXY")) ):
+#    print "#### No GRID PROXY detected. Please do voms-proxy-init -voms cms before submitting Condor jobs ####.\nEXITING"
+#    quit()
+
+
+# *** 1. Create output directory in personal EOS
+if ( not os.path.exists("/eos/uscms/store/user/benjtann/upgrade/samples/{0}/".format(args.outputDir)) ):
+    os.system("mkdir /eos/uscms/store/user/benjtann/upgrade/samples/{0}/".format(args.outputDir))
+
+# *** 2. Create temporary .jdl file for condor submission
+print "\n##########     Submitting Condor jobs     ##########\n"
+
+nEventsSubmittted = 0
+nEventsPerJob = int(args.nTotalEvents / args.nEventsPerJob )
+
+while( nEventsSubmitted < args.nTotalEvents):
+
+    # ** A. Make CMD file for subjob and copy to correct EOS folder
+    subjobRange = '{0}-{1}'.format(nEventsSubmitted, (nEventsSubmitted + nEventsPerJob) )
+    subjobCMDFile = '{0}_{1}.cmd'.format( str(args.inputCMDFile)[:-4], subjobRange) 
+    os.system("cp {0} {1}".format( args.inputCMDFile, subjobCMDFile ))
+    os.system("sed -i 's/set nevents 100/set nevents {0}/g' {1}".format( nEventsPerJob, subjobCMDFile ))
+    os.system("sed -i 's/set iseed 1/set iseed {0}/g' {1}".format( int(nEventsSubmitted / nEventsPerJob) , subjobCMDFile ))
+
+    if ( not os.path.exists("/eos/uscms/store/user/benjtann/upgrade/samples/{0}/{1}".format(args.outputDir, subjobRange)) ):
+        os.system("mkdir /eos/uscms/store/user/benjtann/upgrade/samples/{0}/{1}".format(args.outputDir, subjobRange))
+
+    os.system("xrdcp {0} root://cmseos.fnal.gov//store/user/benjtann/upgrade/samples/{1}/{2}/".format( subjobCMDFIle, args.outputDir, subjobRange))
+
+    
+    # ** B. Submit a job
+    jdl_filename = "submitOneMadgraph_{0}_{1}.jdl".format(args.outputDir, subjobRange)
+
+    os.system("touch {0}".format(jdl_filename))
+    os.system("echo universe = vanilla > {0}".format(jdl_filename))
+    os.system("echo Executable = madgraphGenerate.csh >> {0}".format(jdl_filename))
+    os.system("echo Should_Transfer_Files = YES >> {0}".format(jdl_filename))
+    os.system("echo WhenToTransferOutput = ON_EXIT >> {0}".format(jdl_filename))
+    os.system("echo request_cpus = 2 >> {0}".format(jdl_filename))
+    os.system("echo Transfer_Input_Files = madgraphGenerate.csh >> {0}".format(jdl_filename))
+    os.system("echo Output = {0}/condor_out/outfile_{1}.out  >> {2}".format(args.outputDir, subjobRange, jdl_filename))
+    os.system("echo Error = {0}/condor_err/outfile_{1}.err >> {2}".format(args.outputDir, subjobRange, jdl_filename))
+    os.system("echo Log = {0}/condor_logs/outfile_{1}.log >> {2}".format(args.outputDir, subjobRange, jdl_filename))
+    os.system("echo x509userproxy = ${{X509_USER_PROXY}} >> {0}".format(jdl_filename))
+    os.system("echo Arguments = {0} {1} >> {2}".format( str(args.outputDir+'/'+subjobRange), subjobCMDFile, jdl_filename))
+    os.system("echo Queue 1 >> {0}".format(jdl_filename))       
+    os.system("condor_submit {0}".format(jdl_filename))
+
+    # ** C. Iterate number of events submitted
+    nEventsSubmitted += eventsPerJob
+
+
+# *** 3. Cleanup submission directory
+print "\n##########     Cleanup submission directory     ##########\n"
+os.system("rm *.jdl")
