b94e265e6680b62cf3b2558cf9829603630a0ca7
ctdax <52970623+ctdax@users.noreply.github.com>
2020-08-21 12:25:08 -0400

    Update KerasFF_NeuralNet.py

--- NeuralNetworking/KerasFF_NeuralNet.py
+++ NeuralNetworking/KerasFF_NeuralNet.py
@@ -4,7 +4,8 @@ import FF_NeuralNet_Utils
 import time
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, BatchNormalization
-from keras.callbacks import EarlyStopping
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+from joblib import dump
 
 #Some hyperparameters
 vars = ['deltaR(h1, h2)', 'deltaR(h1 jets)', 'deltaR(h2 jets)', 'hh_mass', 'jet2_eta', 'jet3_eta', 'nJets', 'jet3_pz',
@@ -14,20 +15,21 @@ varlen = len(vars)
 testing_fraction = 0.2
 dropout = 0.2
 l2_reg = l2(0.0001)
-dihiggs_filepath = r"C:\Users\Colby\Desktop\Neu-work\delphes stuff\500kHiggs&2MQCDspreadsheets\ConsiderFirstNjetsInPT=4\higgs\closestDijetMassesToHiggs_higgs.zip"
-qcd_filepath = r"C:\Users\Colby\Desktop\Neu-work\delphes stuff\500kHiggs&2MQCDspreadsheets\ConsiderFirstNjetsInPT=4\qcd\closestDijetMassesToHiggs_qcd.zip"
+dihiggs_filepath = r"C:\Users\Colby\Box Sync\Neu-work\delphes stuff\1MHiggs&4MQCDspreadsheets\higgs sheets\equalDijetMass_higgs.zip"
+qcd_filepath = r"C:\Users\Colby\Box Sync\Neu-work\delphes stuff\1MHiggs&4MQCDspreadsheets\qcd sheets\equalDijetMass_qcd.csv"
+modelcheckpoint_filepath = r"C:\Users\Colby\Box Sync\Neu-work\delphes stuff\1MHiggs&4MQCDspreadsheets\model files\FFNN\bestmodel.hdf5"
 
 #Import files, convert them to dataframes, delte the files
-dihiggs_file, qcd_file = FF_NeuralNet_Utils.extractfiles(dihiggs_filepath, qcd_filepath, is_qcd_multiple_files=True)
+dihiggs_file = FF_NeuralNet_Utils.extractfiles(dihiggs_filepath)
 dfhiggs = read_csv(dihiggs_file)
-#dfqcd = read_csv(qcd_file)
-dfqcd = FF_NeuralNet_Utils.appendQCDfilestogether(qcd_file)
-FF_NeuralNet_Utils.deletefiles(dihiggs_file, qcd_file, is_qcd_multiple_files=True)
+dfqcd = read_csv(qcd_filepath)
+FF_NeuralNet_Utils.deletefiles(dihiggs_file)
 
 #Preprocess data
 data_test, data_train, data_val, label_test, label_train, label_val = FF_NeuralNet_Utils.process(dfhiggs, dfqcd, vars, testing_fraction,
-                                                                                                 nJetsFunction='>=', nJetstoConsider=4,
-                                                                                                 nBTagsFunction='>=', nBTagstoConsider=4)
+                                                                                                 nJetsFunction=False, nJetstoConsider=False,
+                                                                                                 nBTagsFunction=False, nBTagstoConsider=False,
+                                                                                                 equal_qcd_dihiggs_samples=True)
 
 #Start train model timer
 start_time = time.time()
@@ -45,20 +47,21 @@ model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['accuracy'])
 # Setup callbacks
-fit_callbacks = [EarlyStopping(monitor='val_accuracy', mode='max', verbose=0, patience=10, min_delta=0.001,
-                               restore_best_weights=True)]
+fit_callbacks = [EarlyStopping(monitor='val_loss', mode='min', verbose=0, patience=100, min_delta=0.001, restore_best_weights=True),
+                 ModelCheckpoint(filepath=modelcheckpoint_filepath, monitor='val_loss', mode='min', verbose=0, save_best_only=True)]
 # Train model
-history = model.fit(data_train, label_train, validation_data=(data_val, label_val), epochs=100,
-                    callbacks=fit_callbacks, verbose=2)
+history = model.fit(data_train, label_train, validation_data=(data_val, label_val), epochs=50,
+                    callbacks=fit_callbacks, verbose=2, batch_size=1024)
 
 
 #Finish train model timer
 model_time = time.time() - start_time
 
 #Analyze model
-trainscores, testscores = FF_NeuralNet_Utils.accuracy(model, data_train, label_train, data_test, label_test)
-print(trainscores, '\n', testscores)
+#trainscores, testscores = FF_NeuralNet_Utils.accuracy(model, data_train, label_train, data_test, label_test)
+#print(trainscores, '\n', testscores)
 print("--- Train model time %s seconds ---" % model_time)
 FF_NeuralNet_Utils.significance(model, data_test, label_test, testing_fraction)
 FF_NeuralNet_Utils.epoch_history(history)
+FF_NeuralNet_Utils.roc_plot(model, data_test, label_test)
 model.summary()
